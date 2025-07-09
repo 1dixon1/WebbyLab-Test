16183eebf204670a028a2df438acdb6512ef5c68
1dixon1 <02kruk06@gmail.com>
2025-07-09 20:12:16 +0300

    fix

--- src/config/db.js
+++ src/config/db.js
@@ -5,6 +5,15 @@ const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: path.join(__dirname, '../../db.sqlite'),
   logging: false,
+  dialectOptions: {
+    mode: require('sqlite3').OPEN_READWRITE | require('sqlite3').OPEN_CREATE
+  },
+  pool: {
+    max: 1,        
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
 
 module.exports = sequelize;
